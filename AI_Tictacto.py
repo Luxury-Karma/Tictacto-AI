2afc62ffe401692ee540bbfcace853565ce4a3cd
Luxury-Karma <alexgauv@hotmail.ca>
2023-04-28 16:25:24 -0400

    update AI
    Calculate the emplacement of a specific cell to see if it can act or not

--- AI_Tictacto.py
+++ AI_Tictacto.py
@@ -1,16 +1,38 @@
 class AI:
+    '''
+    It will look at the board and look value from 0 to 4. It will choose by the highest value and that's how it will play
+    if 2 value or more have the exact same value it will play one of them randomly
 
-    def __init__(self, board, my_token, ennemi_tokens):
+    ['0', '0', '0', '0', '0'] | ['2', ' ', '2', ' ', '2'] | [' ', ' ', ' ', 'X', ' '] | [' ', ' ', ' ', ' ', 'X']
+    ['0', '2', '2', '2', '0'] | [' ', '1', '1', '1', ' '] | [' ', ' ', ' ', ' ', ' '] | [' ', 'O', ' ', '4', ' ']
+    ['0', '2', '3', '2', '0'] | ['2', '1', 'X', '1', '2'] | [' ', 'O', '3', 'O', ' '] | [' ', '3', 'X', ' ', ' ']
+    ['0', '2', '2', '2', '0'] | [' ', '1', '1', '1', ' '] | [' ', ' ', ' ', ' ', ' '] | [' ', 'O', ' ', ' ', ' ']
+    ['0', '0', '0', '0', '0'] | ['2', ' ', '2', ' ', '2'] | [' ', ' ', ' ', ' ', ' '] | [' ', ' ', ' ', ' ', ' ']
+    This is an exemple with a 5 x 5
+
+    this is an exemple in a
+    3x3
+
+    ['1', '1', '1'] | ['2', '1', '2'] | ['3', '1', '1'] | ['X', '1', 'O'] | ['X', '1', '2'] | ['X', '1', '2']
+    ['1', '3', '1'] | ['1', 'O', '1'] | ['1', 'O', 'X'] | ['4', '1', '3'] | ['1', '1', '1'] | ['1', '0', '1']
+    ['1', '1', '1'] | ['2', '1', '2'] | ['2', '1', 'O'] | ['X', '1', 'O'] | ['2', '1', '2'] | ['2', '0', 'O']
+    '''
+
+    def __init__(self, board, my_token, ennemi_tokens, how_many_token_to_win):
         self.board = board
         self.my_token = my_token
         self.ennemi_tokens = ennemi_tokens
         self.empty_board = board
+        self.how_many_to_win: int = how_many_token_to_win
 
     def board_update(self, new_board):
         self.board = new_board
 
     def copy_empty_board(self):
-        return [['' for _ in range(len(self.empty_board[0]))] for _ in range(len(self.empty_board))]
+        return [[' ' for _ in range(len(self.empty_board[0]))] for _ in range(len(self.empty_board))]
+
+    def copy_empty_board_integer(self):
+        return [[0 for _ in range(len(self.empty_board[0]))] for _ in range(len(self.empty_board))]
 
     def split_tables(self):
         """
@@ -33,3 +55,48 @@ class AI:
         print(p_boards)
         return p_boards
 
+    def check_emplacement_arround_cell(self, cell_emplacement: list[int]):
+        """
+        :param cell_emplacement: where we whant to look
+        :return: list of integer that represent the emplacement left top,down,left,right
+        """
+        board_down_l = len(self.board)
+        board_side_l = len(self.board[0])
+        return [cell_emplacement[0],board_down_l - cell_emplacement[0],cell_emplacement[1], board_side_l - cell_emplacement[1]]
+
+
+
+    def options_evaluation_process(self, individual_board):
+        """
+        will look at the board remove all the allready played move and calculate wich option give
+        the most chance of winning
+        :return: an list of list of integer showing wich one is the best
+        """
+        Unplayable_cells: list[list[int]] = []
+        own_cells: list[[int]] = []
+        # Playing with the string board to look what is happening
+        for e, row in enumerate(self.board):
+            for i, cell in enumerate(row):
+                if cell != ' ':
+                    if cell == self.my_token:
+                        own_cells.append([[e, i]])
+                    else:
+                        Unplayable_cells.append([e, i])
+        # applying what we saw mathematically
+        all_integer_boards: list[list[list[int]]] = []
+        for e in individual_board:
+            integer_board: list[[int]] = self.copy_empty_board_integer()
+            for i,row in enumerate(e):
+                for j, cell in enumerate(row):
+                    # remove the possibility of taking all ready taken case on that board
+                    if cell != ' ':
+                        # If the cell is own by the ennemie
+                        if [i, j] in Unplayable_cells :
+                            integer_board[i][j] = integer_board[i][j] - 100
+
+                        # if the cell is own by my self
+                        if [i, j] in own_cells:
+                            integer_board[i][j] = integer_board[i][j] - 100
+
+
+            all_integer_boards.append([integer_board])
--- AI_Tictacto.pyi
+++ AI_Tictacto.pyi
@@ -1,9 +1,10 @@
 class AI:
-    def __init__(self, board: list[list[str]], my_token: str, ennemi_tokens: list[str]) -> None:
+    def __init__(self, board: list[list[str]], my_token: str, ennemi_tokens: list[str],how_many_token_to_win:int) -> None:
         self.board: list[list[str]] = board
         self.my_token: str = my_token
         self.ennemi_tokens:list[str] = ennemi_tokens
         self.empty_board: list[list[str]] = board
+        self.how_many_to_win: int = how_many_token_to_win
         ...
 
     def board_update(self, new_board: list[list[str]]) -> None:
@@ -13,4 +14,14 @@ class AI:
         ...
 
     def copy_empty_board(self) -> list[list[str]]:
+        ...
+
+    def copy_empty_board_integer(self) -> list[list[int]]:
+        ...
+
+    def check_emplacement_arround_cell(self, cell_emplacement: list[int]) -> list[int]:
+        ...
+
+
+    def options_evaluation_process(self, individual_board: list[list[list[str]]]) -> list[list[int]]:
         ...
