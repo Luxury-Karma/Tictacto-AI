ee212a2d06d114e98f1991768b6de6ff4d801277
Luxury-Karma <alexgauv@hotmail.ca>
2023-05-05 15:06:12 -0400

    speed update

--- test_zone.py
+++ test_zone.py
@@ -1,16 +1,12 @@
-import json
 import statistics
-import time
+
 import Tictacto as ti
 import minMax_AI_Tictac as minMax
-from multiprocessing import Pool, Manager
-from functools import partial
-import math
 from modules.utils import *
-
 import copy
-from typing import Dict, List,Tuple
-
+import json
+from json.decoder import JSONDecodeError
+from collections import deque
 
 moves_done = 0
 elapse = []
@@ -110,15 +106,15 @@ def prepare_next_move(board, move, player_key, player_token, memo, amount_to_win
 
 
 
-def update_progress(moves_done, max_depth, total_possibilities, board):
+def update_progress(moves_done, max_depth, total_possibilities):
 
     r = "\\", "|", "/", "-"
-    #expected_move_still_needed = total_possibilities - moves_done
+    expected_move_still_needed = total_possibilities - moves_done
 
-    #if not elapse:  # Check if elapse is empty
-    #    t = 0
-    #else:
-    #    t = (statistics.median(elapse) * expected_move_still_needed)
+    if not elapse:  # Check if elapse is empty
+        t = 0
+    else:
+        t = (statistics.median(elapse) * expected_move_still_needed)
 
     clear_screen()
     print(f'{r[moves_done % 4]} calculating expected time , expected_move_still_needed ')
@@ -283,70 +279,74 @@ def calculate_board_worth(board: list[list[str]], amount_to_win: int, player_tok
     return total_board_worth
 
 
-import json
-from json.decoder import JSONDecodeError
+
+
+def board_to_tuple(board):
+    return tuple(tuple(row) for row in board)
 
 def generate_json_data(board: list[list[str]], depth: int, amount_to_win: int, player_token: dict, player_key: int,
                        id: int) -> None:
     global moves_done
     global elapse
-    board_data = [board]
+    board_data = deque([board])
     actual_depth = 0
     filename = f'{len(board)}x{len(board)}.json'
     processed_boards = set()
 
+    # Read the existing data from the file
+    try:
+        with open(filename, 'r') as infile:
+            existing_data = json.load(infile)
+    except FileNotFoundError:
+        existing_data = {}
+    except JSONDecodeError:
+        print(f'Error: The JSON file {filename} is broken. Skipping this file.')
+        existing_data = {}
+
     while depth > 0:
-        for depth_board in board_data:
+        for _ in range(len(board_data)):
+            depth_board = board_data.popleft()
             all_possible_move = minMax.generate_moves(board)
-            json_update_data = []
 
             # Calculation for one depth
             for row, cell in all_possible_move:
                 board_copy = copy.deepcopy(depth_board)
                 board_copy[row][cell] = player_token[player_key]["token"]
 
+                board_tuple = board_to_tuple(board_copy)
+                mirrored_board_tuples = [board_tuple] + [board_to_tuple(b) for b in get_mirrored_versions(board_copy)]
+
                 # Check if the board or its mirrored version has been processed
-                if any(tuple(map(tuple, b)) in processed_boards for b in [board_copy] + get_mirrored_versions(board_copy)):
+                if any(b in processed_boards for b in mirrored_board_tuples):
                     continue
 
                 board_worth = calculate_board_worth(board_copy, amount_to_win, player_token, player_key)  # Give how much the board worth
 
                 json_data = create_data_structure(str(id), board_copy, actual_depth, amount_to_win, board_worth, player_token[player_key]["token"]) # Format the data
-                json_update_data.append(json_data)  # Keep all the json formated data for one depth
+
+                # Combine the existing data with the new data
+                key = list(json_data.keys())[0]  # Get the key of the current data dictionary
+                existing_data[key] = json_data[key]  # Add the current data to the existing data using the key
 
                 if not json_data[str(id)]["Winning"]:
                     board_data.append(board_copy)
 
                 # Mark the board as processed
-                processed_boards.add(tuple(map(tuple, board_copy)))
+                processed_boards.add(board_tuple)
 
                 id += 1
-            board_data.remove(depth_board)  # remove the board already looked at
-            # Lets save at each depth
-            # Read the existing data from the file
-        try:
-            with open(filename, 'r') as infile:
-                existing_data = json.load(infile)
-        except FileNotFoundError:
-            existing_data = {}
-        except JSONDecodeError:
-            print(f'Error: The JSON file {filename} is broken. Skipping this file.')
-            existing_data = {}
 
-        # Combine the existing data with the new data
-        for data in json_update_data:
-            key = list(data.keys())[0]  # Get the key of the current data dictionary
-            existing_data[key] = data[key]  # Add the current data to the existing data using the key
-
-        # Save the updated data back to the file
-        with open(filename, 'w') as outfile:
-            json.dump(existing_data, outfile, indent=4)
-        print(f'Saving we are at : {id}')
         actual_depth += 1
         player_key += 1
         if player_key > len(player_token):
             player_key = 1
         depth -= 1
+
+    # Save the updated data back to the file
+    with open(filename, 'w') as outfile:
+        json.dump(existing_data, outfile, indent=4)
+    print(f'Saving we are at : {id}')
+
 
 
 
@@ -360,12 +360,12 @@ from multiprocessing.pool import ThreadPool
 
 
 def main():
-    size = 3
+    size = 5
     amount_to_win = 3
     players_token = {1: {'token': 'X', 'amount played': 0}, 2: {'token': 'O', 'amount played': 0}}
     player_key = 1
     board = ti.board_creation(size)
-    max_depth = 10
+    max_depth = 4
     first_id = 0
     generate_json_data(board, max_depth, amount_to_win, players_token, player_key, first_id)
 
