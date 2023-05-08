ef56058a48384289bdc6c758fc0e8ac922b41a01
Luxury-Karma <alexgauv@hotmail.ca>
2023-05-08 14:27:52 -0400

    update

--- test_zone.py
+++ test_zone.py
@@ -20,7 +20,7 @@ def get_key(board):
     return ''.join([''.join(row) for row in board])
 
 
-def create_data_structure(key:str, board:list[list[str]], depth: int, amount_to_win: int, point_of_the_board:int, player_token: str):
+def create_data_structure(key:str, board:list[list[str]], depth: int, amount_to_win: int, point_of_the_board:int, player_token: str, MotherID:str):
     """
     How the data will look like in the JSON file
     :param key: name of the variable
@@ -34,10 +34,10 @@ def create_data_structure(key:str, board:list[list[str]], depth: int, amount_to_
         key: {
             "Board": board if ''.join([''.join(row) for row in board]).strip() else [],
             "Depth": depth,
-            "ID": key,
             "Winning": ti.three_case_winning(board, amount_to_win)[0],
             "Player": player_token,
             "point of the move": point_of_the_board,
+            "Mother ID": MotherID
         }
     }
     return t if all(t) else None
@@ -201,12 +201,13 @@ def calculate_board_worth(board: list[list[str]], amount_to_win: int, player_tok
 def board_to_tuple(board):
     return tuple(tuple(row) for row in board)
 
+#TODO:MAKE IT EXADECIMAL WITH PART OF THE NUMBER BEING THE DEPTH
 
 def generate_json_data(board: list[list[str]], depth: int, amount_to_win: int, player_token: dict, player_key: int,
                        id: int) -> None:
     global moves_done
     global elapse
-    board_data = deque([board])
+    board_data = deque([(board, None)])  # Add the Mother ID as the second element of the tuple
     actual_depth = 0
     filename = f'{len(board)}x{len(board)}.json'
     processed_boards = set()
@@ -222,9 +223,8 @@ def generate_json_data(board: list[list[str]], depth: int, amount_to_win: int, p
         existing_data = {}
 
     while depth > 0:
-        existing_data[f'Depth {actual_depth}'] = {}
         for _ in range(len(board_data)):
-            depth_board = board_data.popleft()
+            depth_board, mother_id = board_data.popleft()  # Unpack the tuple to get the board and its Mother ID
             all_possible_move = minMax.generate_moves(depth_board)
 
             # Calculation for one depth
@@ -241,14 +241,14 @@ def generate_json_data(board: list[list[str]], depth: int, amount_to_win: int, p
 
                 board_worth = calculate_board_worth(board_copy, amount_to_win, player_token, player_key)  # Give how much the board worth
 
-                json_data = create_data_structure(str(id), board_copy, actual_depth, amount_to_win, board_worth, player_token[player_key]["token"]) # Format the data
+                json_data = create_data_structure(f'{id:0X}', board_copy, actual_depth, amount_to_win, board_worth, player_token[player_key]["token"], mother_id) # Format the data
 
                 # Combine the existing data with the new data
                 key = list(json_data.keys())[0]  # Get the key of the current data dictionary
-                existing_data[f'Depth {actual_depth}'][key] = json_data[key]  # Add the current data to the existing data using the key
+                existing_data[key] = json_data[key]  # Add the current data to the existing data using the key
 
-                if not json_data[str(id)]["Winning"]:
-                    board_data.append(board_copy)
+                if not json_data[f'{id:0X}']["Winning"]:
+                    board_data.append((board_copy, key))  # Add the current key as the Mother ID
 
                 # Mark the board as processed
                 processed_boards.add(board_tuple)
@@ -272,7 +272,6 @@ def generate_json_data(board: list[list[str]], depth: int, amount_to_win: int, p
 
 
 
-
 #--------------------------------------------------------------------------------------------
 
 
@@ -285,7 +284,7 @@ def main():
     players_token = {1: {'token': 'X', 'amount played': 0}, 2: {'token': 'O', 'amount played': 0}}
     player_key = 1
     board = ti.board_creation(size)
-    max_depth = 4
+    max_depth = 13
     first_id = 0
     generate_json_data(board, max_depth, amount_to_win, players_token, player_key, first_id)
 
