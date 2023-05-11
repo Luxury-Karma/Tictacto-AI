dfa418274bc913c2367506c98da1551080730137
Luxury-Karma <108990098+Luxury-Karma@users.noreply.github.com>
2023-05-11 09:53:27 -0400

    update

--- tictacto_Upgrade_C++/tictacto_Upgrade_C++/tictacto_Upgrade_C++.cpp
+++ tictacto_Upgrade_C++/tictacto_Upgrade_C++/tictacto_Upgrade_C++.cpp
@@ -20,12 +20,14 @@ int** create2DArray(int arraySize)
     return arr;
 }
 
+
+// Apply a modification to a 2D array
 int** applyMove(int playerToken, int** board, int row, int col) {
     board[row][col] = playerToken;
     return board;
 }
 
-
+// Show the board on CMD
 void printBoard(int** board, int boardSize) {
     for (int i = 0; i < boardSize; i++) {
         for (int j = 0; j < boardSize; j++) {
@@ -36,23 +38,57 @@ void printBoard(int** board, int boardSize) {
 
 }
 
+// Give the user the choice of the movement
+int* playerMovement() {
+    int answer[2];
+    std::cout << "Enter the row ";
+    std::cin >> answer[0];
+    std::cout << "You entered the row: " << answer[0] << std::endl;
+
+    std::cout << "Enter the col: ";
+    std::cin >> answer[1];
+    std::cout << "You entered the row: " << answer[1] << std::endl;
+
+    return answer;
+}
+
+
 int main()
 {
     int boardSize = 3;
     // initial playing board
     int** board = create2DArray(boardSize);
+    bool playing = 1;
     
     // player board Tokens
     const int playerAmount = 2;
     int player[playerAmount];
     player[0] = 1; 
     player[1] = 2;
-    board = applyMove(player[0], board, 0, 0);
-    printBoard(board, boardSize);
+    int playerIndex = 0;
+    while (playing)
+    {
+ 
+        // player turn
+        int* movement = playerMovement();
+
+        // apply the movement
+        board = applyMove(player[playerIndex], board, movement[0], movement[1]);
+        // remove for memory 
+        delete movement;
+
+
+        //look for winning conditions
+
+        // Insure that the player don't break index and switch players
+        playerIndex++;
+        if (playerAmount <= playerIndex) {
+            playerIndex = 0;
+        }
 
 
 
-
+    }
 
 
 
